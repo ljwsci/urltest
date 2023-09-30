849b654cedfe8189547bf545337ca4c8b4016012
liujiawen2018phyche <97917263+liujiawen2018phyche@users.noreply.github.com>
2023-09-30 18:43:04 +0800

    Add files via upload

---
+++ url1.m
@@ -0,0 +1,11 @@
+MICROSOFT
+Microsoft Evaluation Center = https://www.microsoft.com/zh-cn/evalcenter
+system = https://www.microsoft.com/zh-cn/software-download/
+Windows Server = https://www.microsoft.com/zh-cn/windows-server
+SQL Server = https://www.microsoft.com/zh-cn/sql-server/
+Visual Studio = https://visualstudio.microsoft.com/zh-hans/subscriptions/
+Azure = https://portal.azure.com/
+Github = https://github.com/
+Download = https://www.microsoft.com/en-us/download
+Office 365 = https://www.office.com/
+Onedrive = https://www.microsoft.com/zh-cn/microsoft-365/onedrive/onedrive-for-business
---
+++ url2.m
@@ -0,0 +1,12 @@
+MathWorks
+Matlab = https://www.mathworks.com/
+cnurl = ww2.mathworks.cn/
+Jetbrains
+url = https://www.jetbrains.com
+cnurl = https://www.jetbrains.com.cn
+MySQL
+url = https://dev.mysql.com/downloads/
+VMware
+download = https://customerconnect.vmware.com/en/downloads/#all_products
+Oracle
+java = https://www.oracle.com/java/technologies/downloads/
